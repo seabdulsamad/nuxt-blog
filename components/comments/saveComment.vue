ffc8cd9a18a42f66d93373d08a7022a7bb263cae
Abdul Samad <se.abdulsamad@gmail.com>
2021-06-08 20:45:29 +0400

    Save a new comment feature component.

---
+++ components/comments/saveComment.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-textarea
+    rows="1"
+    auto-grow
+    label="Write your comment here...."
+    append-icon="mdi-send"
+    @click:append="onSaveComment"
+    v-model="params.text"
+    :loading="loading"
+  />
+</template>
+<script>
+export default {
+  props: { postId: { type: Number, required: true } },
+  data: () => ({ params: { text: null, postId: null }, loading: false }),
+  created() {
+    this.params.postId = this.postId;
+  },
+  methods: {
+    onSaveComment() {
+      if (_.size(this.text)) {
+        this.loading = true;
+        this.$axios
+          .post(`/comments`, this.params)
+          .then((res) => (this.comments = res.data))
+          .finally(() => {
+            this.loading = false;
+            this.params.text = null;
+            this.$emit("onPostedComment");
+          });
+      }
+    },
+  },
+};
+</script>
