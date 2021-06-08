ab03ad5a9da7c77f92eb5ea4fc92d4659cdbf9b0
Abdul Samad <se.abdulsamad@gmail.com>
2021-06-08 20:44:30 +0400

    Post detail page implementation

---
+++ pages/posts/_id.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container grid-list-lg v-if="post">
+    <v-flex>
+      <h1 class="headline pb-2">{{ post.title }}</h1>
+      <template v-if="post.user">
+        <v-icon small left>mdi-account</v-icon>{{ post.user.name }}
+      </template>
+      <v-icon small left>mdi-calendar</v-icon>{{ post.created_at }}
+    </v-flex>
+    <p class="py-3">{{ post.description }}</p>
+    <h3 class="text-lg">Comments</h3>
+    <v-flex>
+      <post-comments :postId="$route.query.postId" />
+    </v-flex>
+    <v-flex>
+      <save-comment
+        :postId="$route.query.postId"
+        @onPostedComment="fetchPostById()"
+      />
+    </v-flex>
+  </v-container>
+</template>
+<script>
+import PostComments from "@/components/comments/postsComments";
+import SaveComment from "@/components/comments/saveComment";
+export default {
+  components: { "post-comments": PostComments, "save-comment": SaveComment },
+  data: () => ({ post: null }),
+  created() {
+    this.fetchPostById();
+  },
+  methods: {
+    fetchPostById() {
+      this.$axios
+        .get(`posts/${this.$route.query.postId}`)
+        .then((res) => (this.post = res.data));
+    },
+  },
+};
+</script>
