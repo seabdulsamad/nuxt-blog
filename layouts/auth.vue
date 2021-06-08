b1583826dcccadb2fc31e6bdeadd0318d7e215f0
Abdul Samad <se.abdulsamad@gmail.com>
2021-06-08 20:43:17 +0400

    setup auth layout

---
+++ layouts/auth.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container grid-list-lg>
+        <v-layout row wrap>
+          <v-flex sm9 class="mx-auto">
+            <v-layout class="my-7">
+              <v-flex class="logo-flex-wrap">
+                <nuxt-link to="/" class="text-decoration-none float-left">
+                  <!-- <v-img
+                    :src="`${$appURL}images/logo/CPlogo.png`"
+                    :width="150"
+                  /> -->
+                </nuxt-link>
+              </v-flex>
+            </v-layout>
+            <v-layout row wrap>
+              <v-flex sm6>
+                <nuxt />
+              </v-flex>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
